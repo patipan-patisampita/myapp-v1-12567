d729c21241ce92a900417f340848886d4a5ec2ec
patipan <patipan.patisampita@gmail.com>
2024-08-28 15:42:39 +0700

    updat

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    return res.status(200).json({ status: true, message: 'Home API' })
+})
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+    console.log(`SERVER running at http://localhost${PORT}`)
+})
